77d5e3c27cff5a7fe293a265eb7808eaf5c6b265
Adeel <adeelasakkeer@gmail.com>
2022-06-29 13:13:34 +0530

    added basic hover to draw

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+//Number of pixels = side * side
 let side = 16;
 let grid = document.querySelector(".grid");
+let drawing = false;
 for(let i=0; i<side; i++){
     let row = document.createElement('div');
     row.classList.add('row');
     for(let j=0; j<side; j++){
         let pixel = document.createElement('div');
         pixel.classList.add('pixel');
+        pixel.addEventListener('click',()=>{
+            drawing =  drawing==false? true: false;
+            console.log(drawing);
+        });
+        pixel.addEventListener('mouseover',()=>{
+            if(drawing){
+                pixel.style.backgroundColor = 'black';
+            }
+        })
         row.appendChild(pixel)
     }
     grid.appendChild(row);
