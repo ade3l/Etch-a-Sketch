097c3b7835efaca1e821fa4598680e82d0732ae5
Adeel <adeelasakkeer@gmail.com>
2022-06-29 16:11:07 +0530

    added slider

--- script.js
+++ script.js
@@ -2,22 +2,37 @@
 let side = 16;
 let grid = document.querySelector(".grid");
 let drawing = false;
-for(let i=0; i<side; i++){
-    let row = document.createElement('div');
-    row.classList.add('row');
-    for(let j=0; j<side; j++){
-        let pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        pixel.addEventListener('click',()=>{
-            drawing =  drawing==false? true: false;
-            console.log(drawing);
-        });
-        pixel.addEventListener('mouseover',()=>{
-            if(drawing){
-                pixel.style.backgroundColor = 'black';
-            }
-        })
-        row.appendChild(pixel)
+
+function createGrid(){
+    for(let i=0; i<side; i++){
+        let row = document.createElement('div');
+        row.classList.add('row');
+        for(let j=0; j<side; j++){
+            let pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.addEventListener('click',()=>{
+                drawing =  drawing==false? true: false;
+                console.log(drawing);
+            });
+            pixel.addEventListener('mouseover',()=>{
+                if(drawing){
+                    pixel.style.backgroundColor = 'black';
+                }
+            })
+            row.appendChild(pixel)
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
 }
+function clearGrid(){
+    
+}
+createGrid();
+
+let slider = document.querySelector("#sizeSlider");
+slider.addEventListener('change',()=>{
+    side = slider.value;
+    clearGrid();
+    createGrid();
+});
+slider.addEventListener('input',()=>{console.log(slider.value)});
